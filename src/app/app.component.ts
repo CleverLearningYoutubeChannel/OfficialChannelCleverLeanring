246f629a92ad5e4eeb8afb9eda5a4123a160afde
MOHAMMED-FISAL <fisalc97@gmail.com>
2024-07-04 01:27:11 +0530

    new changes added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { trigger, state, style, animate, transition, keyframes } from '@angular/animations';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ NavbarComponent,CommonModule,FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({
+        opacity: 0
+      })),
+      transition(':enter, :leave', [
+        animate(1000)
+      ])
+    ]),
+    trigger('bounce', [
+      transition('void => *', [
+        animate('1s ease-in', keyframes([
+          style({ transform: 'translateY(-100%)', offset: 0 }),
+          style({ transform: 'translateY(15%)', offset: 0.3 }),
+          style({ transform: 'translateY(-10%)', offset: 0.5 }),
+          style({ transform: 'translateY(0)', offset: 1.0 })
+        ]))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'CleverLearningWebsite';
+  title = 'Clever Learning'.split('');
   imagePath = 'src/public/assets/Image/asp.jpeg';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { AppModule } from './app/app.module';
+
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+
+
+
+
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserAnimationsModule)
+  ]
+}).catch(err => console.error(err));
